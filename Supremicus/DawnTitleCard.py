07c01d9c6579ad18ededf1375749c6038be133e5
Collin Heist <collinheist@gmail.com>
2025-08-04 14:33:12 -0600

    Update EpisodeInfo imports

--- Supremicus/DawnTitleCard.py
+++ Supremicus/DawnTitleCard.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING, Literal, Union
 
 from pydantic import root_validator
 
+from app.info.episode import EpisodeInfo
 from app.logging.logger import log
 from app.schemas.base import BaseCardTypeCustomFontAllText
 from modules.BaseCardType import (
@@ -11,7 +12,6 @@ from modules.BaseCardType import (
     Extra,
     ImageMagickCommands,
 )
-from modules.EpisodeInfo import EpisodeInfo
 from modules.RemoteFile import RemoteFile
 from modules.Title import SplitCharacteristics
 
